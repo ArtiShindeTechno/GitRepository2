202a6717aef3eb663700893705bd7d8b4d1fce51
Arti Shinde <arti.shinde89@gmail.com>
2022-07-29 08:44:53 +0530

    updated class 2 Branch C

--- src/package1/Class2.java
+++ src/package1/Class2.java
@@ -2,7 +2,9 @@ package package1;
 
 public class Class2 {
 
-	public static void main(String args[]) {
-		System.out.println("Created new repository");
+	public static void main(String args[])
+	{
+		System.out.println("Created new repository... BRANCH C");
+		System.out.println("CREATED BRANCH C...");
 	}
 }
