d855d7a030934ba4e7e1ce5855aad7faa6919a15
Arti Shinde <arti.shinde89@gmail.com>
2022-07-29 08:36:37 +0530

    Branch A

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -2,6 +2,7 @@ package package1;
 
 public class Class1 {
 	public static void main(String args[]) {
-		System.out.println("Project upload");
+		System.out.println("Project upload switched to branch A");
+		System.out.println("Project upload branch A...");
 	}
 }
